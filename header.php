95a12c2a7426331ddcbd1f8efc747b0437dbc25f
australiansteve <steve@australiansteve.com>
2015-10-28 23:26:18 -0300

    Add opacity setting to background image customization and actually use the values that are set!

--- header.php
+++ header.php
@@ -19,6 +19,8 @@
 
 <body <?php body_class(); ?>>
 
-<div id="bgImage"/>
+<div id="background-div">
+	<div id="bgImage"/>
+</div>
 
 <div id="page" class="hfeed site">
--- inc/customizer.php
+++ inc/customizer.php
@@ -17,6 +17,7 @@ function hamburgercat_customize_register( $wp_customize ) {
 
 	//All our sections, settings, and controls will be added here
    	$wp_customize->add_setting( 'austeve_background_image' );
+   	$wp_customize->add_setting( 'austeve_background_opacity' );
 
 	$wp_customize->add_section( 'hamburgercat_bg_section' , array(
 	    'title'       => __( 'Background', 'hamburgercat' ),
@@ -36,6 +37,15 @@ function hamburgercat_customize_register( $wp_customize ) {
 		) 
 	);
 
+   	$wp_customize->add_control( 
+   		'austeve_background_opacity', 
+		array(
+			'label'    => __( 'Opacity', 'hamburgercat' ),
+			'section'  => 'hamburgercat_bg_section',
+			'settings' => 'austeve_background_opacity',
+			'type'     => 'text',
+		)
+	);
 
 }
 add_action( 'customize_register', 'hamburgercat_customize_register' );
@@ -47,3 +57,20 @@ function hamburgercat_customize_preview_js() {
 	wp_enqueue_script( 'hamburgercat_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'hamburgercat_customize_preview_js' );
+
+
+/**
+ * Live CSS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function hamburgercat_customize_css()
+{
+    ?>
+        <style type="text/css">
+            #bgImage { 
+             	background-image: url(<?php echo get_theme_mod('austeve_background_image', ''); ?>);
+             	opacity: <?php echo get_theme_mod('austeve_background_opacity', '1.0'); ?>;
+            }
+        </style>
+    <?php
+}
+add_action( 'wp_head', 'hamburgercat_customize_css');
